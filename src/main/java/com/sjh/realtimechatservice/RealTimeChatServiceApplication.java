0076b2b996a031ea32573353b648aa08765dc382
tjwlgns15 <tjwlgns35@naver.com>
2026-06-15 11:04:39 +0900

    first commit

---
+++ src/main/java/com/sjh/realtimechatservice/RealTimeChatServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sjh.realtimechatservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeChatServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealTimeChatServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sjh/realtimechatservice/RealTimeChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sjh.realtimechatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
